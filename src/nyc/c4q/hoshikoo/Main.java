67ad7db9dc908a2e927b880ff0d6a0bd6d1a2a5d
hoshikoo <hoshikooki@gmail.com>
2015-03-27 00:17:26 -0400

    second commit

--- src/nyc/c4q/hoshikoo/Main.java
+++ src/nyc/c4q/hoshikoo/Main.java
@@ -1,8 +1,61 @@
 package nyc.c4q.hoshikoo;
 
+import java.util.Scanner;
+import java.util.ArrayList;
+import java.util.Iterator;
+
 public class Main {
 
     public static void main(String[] args) {
 	// write your code here
+        Scanner input = new Scanner(System.in);
+        ArrayList <Integer> numberList= new ArrayList();
+
+        System.out.println("Give me some numbers to add:");
+
+        while(true){
+            int num = input.nextInt();
+
+            if (num == -2){
+                break;
+            }else{
+                if (num ==-1){
+                    numberList.clear();
+
+                }else{
+                    numberList.add(num);
+                }
+
+            }
+
+
+        }
+
+        int total = 0;
+
+       /* for(int n:numberList){
+            total = total+n;
+        }
+*/
+        //System.out.println(total);
+
+        //this is the same as
+        //for (int i = 0; i < numberList.size(); i++) {
+        //  System.out.println(numberList.get(i) );
+        //}
+
+        //System.out.println(numberList);
+
+
+        Iterator<Integer> numbersIter = numberList.iterator();
+        while (numbersIter.hasNext()) {
+            //numbersIter.next();
+
+
+            total+=numbersIter.next();
+
+        }
+        System.out.println(numberList.size());
+        System.out.println("Thanks! Your sum is "+total);
     }
 }
